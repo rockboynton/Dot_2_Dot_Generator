6d02ff87cbfb3792ee7153c30f961b3c4aa35fa2
rockboynton <rock.boynton@yahoo.com>
2018-03-05 23:52:00 -0600

    GUI set up

---
+++ src/boyntonrl/Controller.java
@@ -0,0 +1,37 @@
+package boyntonrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class Controller {
+
+    @FXML
+    private MenuItem openMenuItem;
+    @FXML
+    private MenuItem closeMenuItem;
+    @FXML
+    private MenuItem linesOnly;
+    @FXML
+    private MenuItem dotsOnly;
+
+    @FXML
+    private void open(ActionEvent e) {
+
+    }
+
+    @FXML
+    private void close(ActionEvent e) {
+
+    }
+
+    @FXML
+    private void setLinesOnly(ActionEvent e) {
+
+    }
+
+    @FXML
+    private void setDotsOnly(ActionEvent e) {
+
+    }
+}
